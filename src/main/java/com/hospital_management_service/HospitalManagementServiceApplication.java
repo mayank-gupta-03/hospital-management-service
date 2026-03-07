1c52741946686e013efa1407c92b436b51fec38c
mayank-gupta-03 <gupta.mayank2003@gmail.com>
2026-03-07 18:42:08 +0530

    Initialize project

---
+++ src/main/java/com/hospital_management_service/HospitalManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hospital_management_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalManagementServiceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/hospital_management_service/entity/Patient.java
@@ -0,0 +1,31 @@
+package com.hospital_management_service.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 40)
+    private String name;
+
+    private LocalDate birthDate;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String gender;
+
+}
